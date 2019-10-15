ac307adc9876e55aa02e6e3248ec3e350473035f
lexypakzaban <lexy.pakzaban@kinkaid.org>
2019-10-15 14:52:10 -0500

    started interface for rolling die

--- src/Referee.java
+++ src/Referee.java
@@ -10,50 +10,56 @@ public class Referee
         System.out.println(myBoard);
     }
 
-    public void play()
-    {
+    public void play() {
         boolean rolling = true;
+        boolean gameIsPlaying = true;
         Scanner keyboardReader = new Scanner(System.in);
 
-        while(rolling){
+        while (gameIsPlaying) {
+            while (rolling) {
+                //rolls die and asks user which they want to move
 
-        int dieA = (int) Math.random() * 7;
-        int dieB = (int) Math.random() * 7;
+                int dieA = (int) Math.random() * 7;
+                int dieB = (int) Math.random() * 7;
 
-        System.out.println("You've rolled:");
-        System.out.println("Die A: " + dieA);
-        System.out.println("Die B: " + dieB);
-        System.out.println("Which do you want to move? Type A or B");
+                System.out.println("You've rolled:");
+                System.out.println("Die A: " + dieA);
+                System.out.println("Die B: " + dieB);
+                System.out.println("Which do you want to move? Type A or B");
 
-        rolling = false;
+                rolling = false;
+
+            }
+
+            while (rolling = false) {
+                String move = keyboardReader.next();
+
+                if (move.equals("A")) {
+                    System.out.println("Which square is that piece currently on?");
+                    int currentSquare = keyboardReader.nextInt();
+                    //TODO: check to see if that piece is in a safe path
+
+                    //TODO: move that piece
+
+                    System.out.println("Ok. The piece on " + currentSquare + " has moved to ");
+
+                    rolling = true;
+                }
+
+                else {
+                    System.out.println("Which square is that piece currently on?");
+                    int currentSquare = keyboardReader.nextInt();
+                    //TODO: check to see if that piece is in a safe path
+
+                    //TODO: move that piece
+
+                    System.out.println("Ok. The piece on " + currentSquare + " has moved to ");
+                    rolling = true;
+
+                }
+
+            }
+
         }
-
-        String move = keyboardReader.next();
-
-        if (keyboardReader.next().equals("A")){
-            System.out.println("Which square is that piece currently on?");
-            int currentSquare = keyboardReader.nextInt();
-
-            //TODO: move that piece
-
-            System.out.println("Ok. The piece on " + currentSquare + " has moved to " );
-
-            rolling = true;
-
-
-
-        }
-
-        if (keyboardReader.next().equals("B")){
-
-        }
-
-        else {
-            System.out.println("That is not a valid response");
-        }
-
-
-
-
     }
 }
