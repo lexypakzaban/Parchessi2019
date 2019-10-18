38f6b01401c2ace4d21ca6d367faab94f6bb4665
lexypakzaban <lexy.pakzaban@kinkaid.org>
2019-10-18 15:10:52 -0500

    fixed playGame

--- src/Referee.java
+++ src/Referee.java
@@ -11,73 +11,73 @@ public class Referee
     }
 
     public void play() {
-//        boolean rolling = true;
-//        boolean gameIsPlaying = true;
-//        int dieA = 0;
-//        int dieB = 0;
-//        Scanner keyboardReader = new Scanner(System.in);
-//
-//        while (gameIsPlaying) {
-//
-//            for (int playerNum = 0; playerNum < 4; playerNum++) {
-//                //loops through every player
-//                System.out.println("Player " + playerNum + " 's turn!");
-//
-//                while (rolling) {
-//                    //rolls die and asks user which they want to move
-//
-//                    dieA = (int) Math.random() * 7;
-//                    dieB = (int) Math.random() * 7;
-//
-//                    System.out.println("You've rolled:");
-//                    System.out.println("Die A: " + dieA);
-//                    System.out.println("Die B: " + dieB);
-//                    System.out.println("Which do you want to move? Type A or B");
-//
-//                    rolling = false;
-//
-//                }
-//
-//                while (rolling = false) {
-//                    String move = keyboardReader.next();
-//
-//                    if (move.equals("A")) {
-//                        System.out.println("Which space is that piece currently on?");
-//                        int currentSpace = keyboardReader.nextInt();
-//                        //TODO: check to see if that piece is in a safe path
-//
-//                        //moves that piece
-//                        int newSpace = currentSpace + dieA;
-//                        myBoard.moveToANewSpace(newSpace, playerNum);
-//
-//                        //clears old space (make sure that piece isn't on its old space)
-//                        myBoard.clearOldSpace(currentSpace, playerNum);
-//
-//                        System.out.println("Ok. The piece on " + currentSpace + " has moved to " + newSpace);
-//
-//                        rolling = true;
-//                    }
-//
-//                    else {
-//                        System.out.println("Which space is that piece currently on?");
-//                        int currentSpace = keyboardReader.nextInt();
-//                        //TODO: check to see if that piece is in a safe path
-//
-//                        //moves that piece
-//                        int newSpace = currentSpace + dieB;
-//                        myBoard.moveToANewSpace(newSpace, playerNum);
-//
-//                        //clears old space (make sure that piece isn't on its old space)
-//                        myBoard.clearOldSpace(currentSpace, playerNum);
-//
-//                        System.out.println("Ok. The piece on " + currentSpace + " has moved to ");
-//                        rolling = true;
-//
-//                    }
-//
-//                }
-//
-//            }
-//        }
+        boolean rolling = true;
+        boolean gameIsPlaying = true;
+        int dieA = 0;
+        int dieB = 0;
+        Scanner keyboardReader = new Scanner(System.in);
+
+        while (gameIsPlaying) {
+
+            for (int playerNum = 0; playerNum < 4; playerNum++) {
+                //loops through every player
+                System.out.println("Player " + playerNum + " 's turn!");
+
+                while (rolling) {
+                    //rolls die and asks user which they want to move
+
+                    dieA = (int) (Math.random() * 7);
+                    dieB = (int) (Math.random() * 7);
+
+                    System.out.println("You've rolled:");
+                    System.out.println("Die A: " + dieA);
+                    System.out.println("Die B: " + dieB);
+                    System.out.println("Which do you want to move? Type A or B");
+
+                    rolling = false;
+
+                }
+
+                while (rolling == false) {
+                    String move = keyboardReader.next();
+
+                    if (move.equals("A")) {
+                        System.out.println("Which space is that piece currently on?");
+                        int currentSpace = keyboardReader.nextInt();
+                        //TODO: check to see if that piece is in a safe path
+
+                        //moves that piece
+                        int newSpace = currentSpace + dieA;
+                        myBoard.moveToANewSpace(newSpace, playerNum);
+
+                        //clears old space (make sure that piece isn't on its old space)
+                        myBoard.clearOldSpace(currentSpace, playerNum);
+
+                        System.out.println("Ok. The piece on " + currentSpace + " has moved to " + newSpace);
+
+                        rolling = true;
+                    }
+
+                    else {
+                        System.out.println("Which space is that piece currently on?");
+                        int currentSpace = keyboardReader.nextInt();
+                        //TODO: check to see if that piece is in a safe path
+
+                        //moves that piece
+                        int newSpace = currentSpace + dieB;
+                        myBoard.moveToANewSpace(newSpace, playerNum);
+
+                        //clears old space (make sure that piece isn't on its old space)
+                        myBoard.clearOldSpace(currentSpace, playerNum);
+
+                        System.out.println("Ok. The piece on " + currentSpace + " has moved to ");
+                        rolling = true;
+
+                    }
+
+                }
+
+            }
+        }
     }
 }
